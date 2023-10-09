addd852e61559f9a57f2c770172c52cb749b7f32
AhmedAbdAlZaher <ahmed.abdalzaher@outlook.com>
2023-10-09 18:56:18 +0300

    sarch

--- src/Components/Catagories/Catagories.jsx
+++ src/Components/Catagories/Catagories.jsx
@@ -1,7 +1,56 @@
 import React from 'react'
 import styles from "./Catagories.module.css"
 export default function Catagories() {
+
+
+
+
+
+
+
+
+
+  
   return (
-    <div>Catagories</div>
+    <>
+      <div className=' container'>
+        <div className="row">
+          <div className="col-md-4">
+          <div className="card">
+            <img src="..." className="card-img-top" alt="..."/>
+              <div className="card-body">
+                <h2 className="card-text">Music</h2>
+              </div>
+              </div>
+          </div>
+          <div className="col-md-4">
+          <div className="card ">
+            <img src="..." className="card-img-top" alt="..."/>
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+              </div>
+          </div>
+          <div className="col-md-4">
+          <div className="card">
+            <img src="..." className="card-img-top" alt="..."/>
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+              </div>
+          </div>
+          <div className="col-md-4">
+          <div className="card">
+            <img src="..." className="card-img-top" alt="..."/>
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+              </div>
+          </div>
+        </div>
+
+      </div>
+
+    </>
   )
 }
