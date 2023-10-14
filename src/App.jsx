4eb731a78aa3863ee9fdd7c25403318b2199fffb
AhmedAbdAlZaher <ahmed.abdalzaher@outlook.com>
2023-10-14 12:45:45 +0300

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
- import './App.css';
+import './App.css';
 import Home from './Components/Home/Home';
 import Layout from './Components/Layout/Layout';
-
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {  BrowserRouter, RouterProvider, createHashRouter } from 'react-router-dom';
 import Products from './Components/Products/Products';
 import Catagories from './Components/Catagories/Catagories';
 import Register from './Components/Register/Register';
@@ -20,41 +19,44 @@ import Wishlist from './Components/WishList/WishList';
 
 
 
-let routers = createBrowserRouter([
+let routers = createHashRouter([
   {
-  path:"/",element:<Layout></Layout>,children:[
-    {index:true, element:  <ProtectedRoutes><Home></Home></ProtectedRoutes>},
-    {path:"products", element:<ProtectedRoutes><Products></Products></ProtectedRoutes>},
-    {path:"Catagories", element: <ProtectedRoutes><Catagories></Catagories></ProtectedRoutes>},
-    {path:"brands", element:<ProtectedRoutes><Brands></Brands></ProtectedRoutes>},
-    {path:"cart", element:<ProtectedRoutes><Cart></Cart></ProtectedRoutes>},
-    {path:"details/:id", element:<ProtectedRoutes><Details></Details></ProtectedRoutes>},
-    {path:"Cheakout", element:<ProtectedRoutes><Cheakout></Cheakout></ProtectedRoutes>},
-    {path:"allorders", element:<ProtectedRoutes><AllOrders></AllOrders></ProtectedRoutes>},
-    {path:"wishlist", element:<ProtectedRoutes><Wishlist></Wishlist></ProtectedRoutes>},
-
-    
-    {path:"register", element:<Register></Register>},
-    {path:"login", element:<Login></Login>},
-
-    {path:"*", element:<NotFound></NotFound>},
+    path: "/", element: <Layout></Layout>, children: [
+      { index: true, element: <ProtectedRoutes><Home></Home></ProtectedRoutes> },
+      { path: "products", element: <ProtectedRoutes><Products></Products></ProtectedRoutes> },
+      { path: "Catagories", element: <ProtectedRoutes><Catagories></Catagories></ProtectedRoutes> },
+      { path: "brands", element: <ProtectedRoutes><Brands></Brands></ProtectedRoutes> },
+      { path: "cart", element: <ProtectedRoutes><Cart></Cart></ProtectedRoutes> },
+      { path: "details/:id", element: <ProtectedRoutes><Details></Details></ProtectedRoutes> },
+      { path: "Cheakout", element: <ProtectedRoutes><Cheakout></Cheakout></ProtectedRoutes> },
+      { path: "allorders", element: <ProtectedRoutes><AllOrders></AllOrders></ProtectedRoutes> },
+      { path: "wishlist", element: <ProtectedRoutes><Wishlist></Wishlist></ProtectedRoutes> },
 
 
-  ]}
+      { path: "register", element: <Register></Register> },
+      { path: "login", element: <Login></Login> },
+
+      { path: "*", element: <NotFound></NotFound> },
+
+
+    ]
+  }
 ])
 
 
 
 
 function App() {
-let {setToken} = useContext(tokenContext)
+  let { setToken } = useContext(tokenContext)
   useEffect(() => {
-if(localStorage.getItem("userToken")){
-  setToken(localStorage.getItem("userToken"))
-}
-  },[])
+    if (localStorage.getItem("userToken")) {
+      setToken(localStorage.getItem("userToken"))
+    }
+  }, [])
   return (
-  <RouterProvider router={routers}></RouterProvider>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <RouterProvider router={routers}></RouterProvider>
+    </BrowserRouter>
   )
 }
 
