d2d067b3a36ea071ff5c1d3398c486308c3a958f
AhmedAbdAlZaher <ahmed.abdalzaher@outlook.com>
2023-10-31 00:08:24 +0200

    sd

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -65,7 +65,7 @@ export default function CartContextProvider(props) {
 
 
     function onlinePayment(shippingAddress) {
-        return axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=http://localhost:3000`,
+        return axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=https://AhmedAbdAlZaher.github.io/FreshCart`,
             {
                 shippingAddress,
             },
